17581705a60cddc009cb6a08260f9a56a7175ea2
jasonlin15 <jasonlin1@ufl.edu>
2025-09-23 21:42:17 -0400

    Adding some screen...

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { Login } from './login/login'
+import { Signup } from './signup/signup'
+import { Dashboard } from './dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/login', pathMatch: 'full'},
+    {path: 'login', component: Login},
+    {path: 'signup', component: Signup},
+    {path: 'Dashboard', component: Dashboard}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
---
+++ frontend/src/app/dashboard/dashboard.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, Router} from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [RouterLink],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css'
+})
+export class Dashboard {
+
+}
